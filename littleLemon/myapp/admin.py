473864e25b169d8b48b177346528f0985f8c9cff
abid ullah <abidfazal1166@gmail.com>
2025-06-12 08:37:55 +0500

    database and model configurations

--- littleLemon/myapp/admin.py
+++ littleLemon/myapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Menu, Booking
 
 # Register your models here.
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'inventory')
+    search_fields = ('name',)
+    
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'booking_date', 'no_of_guests')
+    search_fields = ('name',)
+    
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(Booking, BookingAdmin)
---
+++ littleLemon/myapp/file.sql
@@ -0,0 +1,1 @@
+SELECT * FROM `reservations`.`myapp_menu` LIMIT 1000;
